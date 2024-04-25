d6c6d33a75574f8b687a03e836f8fcb924d42888
Raziah <abdullahiraziah@gmail.com>
2024-04-25 09:22:31 +0300

    worked on stacks

---
+++ stacks.py
@@ -0,0 +1,18 @@
+#// adding elements in a stack
+stack1 = []
+stack1.append("Raziah")
+stack1.append("Abdullahi")
+stack1.append("Mohamed")
+print(stack1)
+
+stack2 = []
+stack2.append("tomatoes")
+stack2.append("onions")
+stack2.append("carrots")
+print(stack2)
+
+# // removing items from a stack
+stack1.pop()
+print(stack1.pop())
+
+print(stack2.pop())
